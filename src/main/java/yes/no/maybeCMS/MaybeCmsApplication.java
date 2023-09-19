3e068ea4dc5de893d48bad0c436543f68d36e6c1
ABitOfAByte <marcus.migotti@gmail.com>
2023-09-19 13:22:58 +0200

    Initial commit

---
+++ src/main/java/yes/no/maybeCMS/MaybeCmsApplication.java
@@ -0,0 +1,13 @@
+package yes.no.maybeCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaybeCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaybeCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yes/no/maybeCMS/MaybeCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package yes.no.maybeCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaybeCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
