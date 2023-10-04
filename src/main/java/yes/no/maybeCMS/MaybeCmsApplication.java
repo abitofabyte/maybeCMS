4bcbaa29980590cec4bc619a423a26dbdca7a6c2
ABitOfAByte <marcus.migotti@gmail.com>
2023-10-04 16:03:53 +0200

    switch frontend port to 8001 to avoid conflicts, reflect for CORS

--- src/main/java/yes/no/maybeCMS/MaybeCmsApplication.java
+++ src/main/java/yes/no/maybeCMS/MaybeCmsApplication.java
@@ -18,7 +18,7 @@ public class MaybeCmsApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost");
+				registry.addMapping("/**").allowedOrigins("http://localhost:8001");
 			}
 		};
 	}
