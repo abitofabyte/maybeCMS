a34c1124252167ef3e65a9c26efec9230a2de15e
ABitOfAByte <marcus.migotti@gmail.com>
2023-10-05 16:26:19 +0200

    refactoring to services requires a lot of exception handling

--- src/main/java/yes/no/maybeCMS/controllers/shop/products/ProductRestController.java
+++ src/main/java/yes/no/maybeCMS/controllers/shop/products/ProductRestController.java
@@ -1,6 +1,10 @@
 package yes.no.maybeCMS.controllers.shop.products;
 
 import org.springframework.web.bind.annotation.*;
+import yes.no.maybeCMS.services.categories.CategoryNotFoundException;
+import yes.no.maybeCMS.controllers.shop.discounts.DiscountNotFoundException;
+import yes.no.maybeCMS.controllers.shop.inventories.InventoryNotFoundException;
+import yes.no.maybeCMS.controllers.shop.vats.VatNotFoundException;
 import yes.no.maybeCMS.entities.shop.Product;
 import yes.no.maybeCMS.services.products.ProductNotFoundException;
 import yes.no.maybeCMS.services.products.ProductService;
@@ -38,7 +42,7 @@ public class ProductRestController {
     }
 
     @PatchMapping
-    Product update(@RequestBody Product product) throws ProductNotFoundException {
+    Product update(@RequestBody Product product) throws ProductNotFoundException, CategoryNotFoundException, InventoryNotFoundException, DiscountNotFoundException, VatNotFoundException {
         return productService.update(product);
     }
 }
