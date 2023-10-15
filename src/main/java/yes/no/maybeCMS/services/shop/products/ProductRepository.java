7a862bfd3d18ec5b0a1c511951f5f2e400898f3b
ABitOfAByte <marcus.migotti@gmail.com>
2023-10-15 15:09:58 +0200

    find by category added

--- src/main/java/yes/no/maybeCMS/services/shop/products/ProductRepository.java
+++ src/main/java/yes/no/maybeCMS/services/shop/products/ProductRepository.java
@@ -3,6 +3,7 @@ package yes.no.maybeCMS.services.shop.products;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import yes.no.maybeCMS.entities.shop.Category;
 import yes.no.maybeCMS.entities.shop.Product;
 import yes.no.maybeCMS.entities.users.User;
 
@@ -11,4 +12,5 @@ import java.util.UUID;
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     Page<Product> findByNameIgnoreCaseContaining(String name, Pageable pageable);
     Page<Product> findBySeller(User seller, Pageable pageable);
+    Page<Product> findByCategory(Category category, Pageable pageable);
 }
